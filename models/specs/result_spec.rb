b4455945a6af174aca45089654710c7b1ba2eb3a
omikron15 <omikron15@hotmail.co.uk>
2018-04-27 16:06:40 +0100

    Players tests written for basic getter attributes. 0/3 tests passing

---
+++ models/specs/result_spec.rb
@@ -0,0 +1,30 @@
+require 'minitest/autorun'
+require 'minitest/rg'
+require_relative '../Result.rb'
+
+class TestResult < Minitest::Test
+
+  def setup()
+    options1 = {
+      "player_id" => 1,
+      "game_id" => 1,
+      "ammount" => 20
+
+    }
+    @result1 = Result.new(options1)
+
+  end
+
+  def test_player_id()
+    assert_equal(1, @result1.player_id)
+  end
+
+  def test_game_id()
+    assert_equal(1, @result1.game_id)
+  end
+
+  def test_ammount()
+    assert_equal(20, @result1.ammount)
+  end
+
+end #End of TestPlayer class
